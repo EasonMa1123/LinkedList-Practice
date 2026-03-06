9255d132f1fa19479e8d0e1c6934e9d59c8b1d19
Eason Ma <163453353+EasonMa1123@users.noreply.github.com>
2026-03-06 13:15:19 +0000

    Refactor hashTable class for improved functionality

--- hashTable.py
+++ hashTable.py
@@ -1,22 +1,40 @@
 class hashTable:
   
-  def __init__(self,size):
-    self.__table =[[] for _ in range(size)]
-    self.__size = size
+  def __init__(self,intianl_size = 10):
+    self.__table =[[] for _ in range(intianl_size)]
+    self.__ItemAmount = 0 
+    self.__TableSize = intianl_size
 
 
   def __hash(self,value):
-    return (value+10) % self.__size
+    return (value+(int(self.__TableSize/value))) % self.__TableSize
 
-
+  def __ChangeSize(self):
+      new_size = int(((self.__ItemAmount//10)+1))*10
+      self.__ItemAmount = 0 
+      self.__TableSize = new_size
+      tempItemTable = []
+      for i in self.__table:
+        for l in i:
+          tempItemTable.append(l)
+      self.__table = [[] for _ in range(self.__TableSize)]
+      for x in tempItemTable:
+        self.__appendValue(x)
 
   def addValue(self,value):
+    if self.__ItemAmount > self.__TableSize:
+      self.__ChangeSize()
+    self.__appendValue(value)
+
+
+  def __appendValue(self,value):
     hashed_value = self.__hash(value if type(value) == int else sum(ord(i) for i in value))
     if value in self.__table[hashed_value]:
       return
     else:
       self.__table[hashed_value].append(value)
-    
+      self.__ItemAmount += 1
+
 
   def __str__(self):
     return str(self.__table)
